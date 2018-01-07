68c19212bd7ffa4a46c0178f29bc6cf21fe186ef
Harris Jordan <wjordan213@gmail.com>
2018-01-07 09:25:46 -0500

    extract total_frequent_renter points method

--- starting_example/lib/customer.rb
+++ starting_example/lib/customer.rb
@@ -15,20 +15,21 @@ class Customer
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |rental|
-      # add frequent renter points
-      frequent_renter_points += rental.frequent_renter_points
-
       # add bonus for a two day new release rental
       result += "\t" + rental.movie.title + "\t" + rental.charge.to_s + "\n"
     end
 
     # add footer lines
     result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
   end
 
   private
+
+  def total_frequent_renter_points
+    @rentals.sum(&:frequent_renter_points)
+  end
 
   def total_charge
     @rentals.sum(&:charge)
