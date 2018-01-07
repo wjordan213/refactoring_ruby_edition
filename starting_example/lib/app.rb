b6a379cc61b12e1f301618b6c69f47f7e02e7f61
Harris Jordan <wjordan213@gmail.com>
2018-01-06 23:53:24 -0500

    rename this_amount to result and element to rental in amount_for

--- starting_example/lib/app.rb
+++ starting_example/lib/app.rb
@@ -35,17 +35,17 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
 
-    @rentals.each do |element|
-      this_amount = amount_for(element)
+    @rentals.each do |rental|
+      this_amount = amount_for(rental)
 
       # add frequent renter points
       frequent_renter_points += 1
-      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+      if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
         frequent_renter_points += 1
       end
 
       # add bonus for a two day new release rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
+      result += "\t" + rental.movie.title + "\t" + this_amount.to_s + "\n"
 
       total_amount += this_amount
     end
@@ -58,20 +58,20 @@ class Customer
 
   private
 
-  def amount_for(element)
-    this_amount = 0
+  def amount_for(rental)
+    result = 0
 
-    case element.movie.price_code
+    case rental.movie.price_code
     when Movie::REGULAR
-      this_amount += 2
-      this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+      result += 2
+      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
     when Movie::NEW_RELEASE
-      this_amount += element.days_rented * 3
+      result += rental.days_rented * 3
     when Movie::CHILDRENS
-      this_amount += 1.5
-      this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+      result += 1.5
+      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
     end
 
-    this_amount
+    result
   end
 end
