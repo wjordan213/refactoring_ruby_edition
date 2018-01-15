8813c3f913090199d886373ccd30b6c9ee72f4d4
Harris Jordan <wjordan213@gmail.com>
2018-01-15 12:38:46 -0500

    create RegularPrice, NewReleasePrice, and ChildrensPrice

---
+++ starting_example/lib/regular_price.rb
@@ -0,0 +1,2 @@
+class RegularPrice
+end
--- starting_example/spec/spec_helper.rb
+++ starting_example/spec/spec_helper.rb
@@ -5,6 +5,9 @@ SimpleCov.start
 require './lib/customer.rb'
 require './lib/movie.rb'
 require './lib/rental.rb'
+require './lib/regular_price.rb'
+require './lib/new_release_price.rb'
+require './lib/childrens_price.rb'
 
 # %w{ lib/movie lib/rental lib/customer }.each do |file|
 #   require File.expand_path file
