d4d893e71f0e9fac7d47559477a6935c2777821c
Harris Jordan <wjordan213@gmail.com>
2018-01-15 13:07:04 -0500

    delegate charge to movie price

--- starting_example/lib/movie.rb
+++ starting_example/lib/movie.rb
@@ -22,19 +22,7 @@ class Movie
   end
 
   def charge(days_rented)
-    result = 0
-
-    case price_code
-    when Movie::REGULAR
-      return @price.charge(days_rented)
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
--- starting_example/lib/new_release_price.rb
+++ starting_example/lib/new_release_price.rb
@@ -1,2 +1,5 @@
 class NewReleasePrice
+  def charge(days_rented)
+    days_rented * 3
+  end
 end
